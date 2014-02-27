7d2224cf6f21ad05913d6a425dd5de2cfc412190
MBlake <s00132717@mail.itsligo.ie>
2014-02-27 12:30:47 +0000

    Initial commit -first steps

---
+++ MBlake GitHub Trial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBlake_GitHub_Trial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
